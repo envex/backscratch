f178c7b3cb349017a1736739febcf64ca491655a
envex <matt@envexlabs.com>
2012-11-11 22:03:37 -0600

    Commenting a bit

--- jquery.backscratch.js
+++ jquery.backscratch.js
@@ -1,3 +1,16 @@
+/**
+ * Backscratch
+ *
+ * Backscratch is a jQuery plugin that serves up better image sizes when using Backstretch
+ *
+ * @author     Matt Vickers
+ * @link       http://hey.facetdev.com
+ * @copyright  2012
+ * @license    Do what you want -- Just don't resell
+ * @version    1.0
+ * 
+ */
+
 (function( $ ){
 
   $.fn.backscratch = function(available){
@@ -12,6 +25,7 @@
         body = $('body'),
         $bg  = this.find('img.bg');
 
+    // Hide the initial image
     $bg.hide();
 
     win.resize(function() {
@@ -19,6 +33,12 @@
       var win_w = win.width(),
           current = body.data('backscratch-size');
       
+      /**
+       * We are only going to serve up bigger images
+       * because a larger version as already been
+       * loaded.
+       */
+
       if (!current || ((current < win_w) && (current < available[available.length - 1]))) {
         
         var chosen = available[available.length - 1];
@@ -36,8 +56,10 @@
               image = $this.data('src'),
               image_info = image.split('.');
 
+          // Add the current size to the body for reference
           body.attr('data-backscratch-size', chosen);
 
+          // Setup/Reload backstrech on all elements
           $this.parent('.backscratch').backstretch(image_info[0] + '_' + chosen + '.' + image_info[1]);
 
         });
